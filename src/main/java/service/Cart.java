ca20ffc15c0c951ff0fa242a1522d2bfa801a515
Tkakishak <tkakishakd@mail.ru>
2023-06-25 22:02:55 +0300

    final25061

--- src/main/java/service/Cart.java
+++ src/main/java/service/Cart.java
@@ -1,11 +1,12 @@
 package service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-@Component
+@SessionScope
 public class Cart {
     private final List<Integer> items = new ArrayList<>();
     public void addItems(List<Integer> itemsIds){
