cceecb5067f3af6dcf82377d272f5f4624328b83
Tkakishak <tkakishakd@mail.ru>
2023-06-25 22:09:23 +0300

    final25061

--- src/main/java/service/Cart.java
+++ src/main/java/service/Cart.java
@@ -6,6 +6,7 @@ import org.springframework.web.context.annotation.SessionScope;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+@Component
 @SessionScope
 public class Cart {
     private final List<Integer> items = new ArrayList<>();
